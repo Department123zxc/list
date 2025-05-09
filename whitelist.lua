e4ce943676c730ffd4ae6bdb2a2bf87ea1a794b7
PotatoNotTomato <140255333+PotatoEiii@users.noreply.github.com>
2025-05-09 18:47:39 +0700

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -68,7 +68,7 @@ return {
 "5c004c35-b3ae-462c-8be6-1d181c95b8af", -- Trần Bảo Khang
 "C914F29A-70F2-4F48-AC89-93E88DD6FC94", -- Đinh Đức Thành
 "340DCD66-114A-44BB-B61C-7AAE03384C72", -- Thành Đạt
-"35f8b102-c2c8-4119-849f-af382bb43f71", -- Nguyễn Hồng Đạt
+"4ba62768-ed5b-4deb-a4a4-90692134ad71", -- Nguyễn Hồng Đạt
 "---",-- Gia Khánh
 "1c9dedde-4a95-46f7-9835-8abb67018cac" -- Nhật Quang
 }
