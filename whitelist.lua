508e1ac6592d1c951a8eee67174d69199cc1fe82
khoi2107 <135852787+khoi2107@users.noreply.github.com>
2025-05-11 20:36:25 +0700

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -65,7 +65,7 @@ return {
 "394708B8-6BB1-44E8-9BA7-78699705D173", -- Dương Văn Nguyên🌼
 "20b5448c-bfe8-4dd8-a871-1896caeb2164", -- Nguyễn Hữu Duy
 "87C41DEC-3EB0-4148-8A57-6824A44F0CAC", -- Nguyễn Minh Nhật
-"5c004c35-b3ae-462c-8be6-1d181c95b8af", -- Trần Bảo Khang
+"f176034b-f522-4ceb-b757-ed9145650abd", -- Trần Bảo Khang
 "C914F29A-70F2-4F48-AC89-93E88DD6FC94", -- Đinh Đức Thành
 "340DCD66-114A-44BB-B61C-7AAE03384C72", -- Thành Đạt
 "4ba62768-ed5b-4deb-a4a4-90692134ad71", -- Nguyễn Hồng Đạt
