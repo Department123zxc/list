7b5a194d459ae2b8bb1f1be18fc78ceb21cd8b79
PotatoNotTomato <140255333+PotatoEiii@users.noreply.github.com>
2025-07-06 15:56:33 +0700

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -87,7 +87,7 @@ return {
 "48873a4a-9b5c-4486-83d5-c531a0b10059", -- Quý Trương Minh
 "5de56483-8942-44e7-a6e2-d87ce6682379", -- Huỳnh Chung Nam
 "ca292a56-c969-4c93-8d73-5adb80cfdece", --- Phương Loan
-"d1c4f711-5deb-4ac1-9c0c-5992eb434bd9", -- Nguyễn Hữu Đạt
+"18688f92-6f0f-4e72-bded-10aa84900ee9", -- Nguyễn Hữu Đạt
 "7c94ccc0-3d1a-40fa-aa95-d02c5b80ff3b", -- Dray Wakins
 "0dae5822-7bc9-4136-ae77-d4b3502223b1", -- Gia Khánh 
 "062abea6-af1b-440c-9164-ea751e432a7f" -- loading name discord
