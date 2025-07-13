c65e7bf58f6fad92ebe1876d02275aaba5d292c0
khoi2107 <135852787+khoi2107@users.noreply.github.com>
2025-07-13 12:32:56 +0700

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -6,7 +6,7 @@ return {
 "4B050BA0-25E1-446E-BE98-6B8BEFB5F7DF", -- Iren PC
 "25cb3892-3dbb-4e2f-a536-1f23b14de04d", -- Iren PE
 "b2835f66-3d86-4a02-94de-6ee4876c7ef0",
-"e384a6a1-c262-412f-a66f-184d94cfa621",
+"1f01bc65-11ba-4268-8b1f-59c761aaf264",
 "7af0a090-492d-4353-8c4a-b0719c7b6184", -- khoi mumu
 
 
